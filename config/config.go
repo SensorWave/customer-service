9f6e266361f0169ec6133d4425018a12cc98a962
dieperid <david.dieperink@ik.me>
2025-11-17 13:44:53 +0100

    chore: fixed typo

--- config/config.go
+++ config/config.go
@@ -1,39 +1,39 @@
 package config
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 )
 
 type Config struct {
-    DBHost     string
-    DBUser     string
-    DBPassword string
-    DBName     string
-    RabbitMQ   string
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	RabbitMQ   string
 }
 
 func LoadConfig() *Config {
-    cfg := &Config{
-        DBHost: os.Getenv("DB_HOST"),
-        DBUser: os.Getenv("DB_USER"),
-        DBPassword: os.Getenv("DB_PASSWORD"),
-        DBName: os.Getenv("DB_NAME"),
-        RabbitMQ: os.Getenv("RABBITMQ_HOST"),
-    }
+	cfg := &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		RabbitMQ:   os.Getenv("RABBITMQ_HOST"),
+	}
 
-    if cfg.DBHost == "" || cfg.DBUser == "" || cfg.DBPassword == "" || cfg.DBName == "" || cfg.RabbitMQ == "" {
-        log.Fatal("Missing required environment variables")
-    }
+	if cfg.DBHost == "" || cfg.DBUser == "" || cfg.DBPassword == "" || cfg.DBName == "" || cfg.RabbitMQ == "" {
+		log.Fatal("Missing required environment variables")
+	}
 
-    return cfg
+	return cfg
 }
 
 func (c *Config) PostgresDSN() string {
-    return fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", c.DBHost, c.DBUser, c.DBPassword, c.DBName)
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", c.DBHost, c.DBUser, c.DBPassword, c.DBName)
 }
 
 func (c *Config) RabbitMQURL() string {
-    return fmt.Sprintf("amqp://%s:5672/", c.RabbitMQ)
+	return fmt.Sprintf("amqp://%s:5672/", c.RabbitMQ)
 }
